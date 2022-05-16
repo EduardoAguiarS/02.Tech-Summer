5d4fa2ddfd5c680cd0ac600a021d0586e477ac67
Eduardo Aguiar <edu.pessoal@outlook.com>
2022-05-16 11:17:25 -0300

    styles globals and variables

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Product } from "./pages/Product";
-import { useProduct } from "./providers/context";
 
 export const AppRoutes = () => {
-  const { product } = useProduct();
-
   return (
     <BrowserRouter>
       <Routes>
