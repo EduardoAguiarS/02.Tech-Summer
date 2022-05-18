04be3997cc9f4d947985f70ff3643d223d0b11bc
Eduardo Aguiar <edu.pessoal@outlook.com>
2022-05-18 11:15:17 -0300

    Product Color

---
+++ src/components/ProductColors/index.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { useProduct } from "../../providers/context";
+// import "./styles.scss";
+
+export const ProductColors = () => {
+  const { product } = useProduct();
+  const [color, setColor] = useState<string>("");
+
+  return (
+    <div className="color">
+      <div className="color__name">
+        <span>Cor:</span>
+        <span className="color__value">{color}</span>
+      </div>
+
+      {product?.skuSpecifications[0].values.map((value, index) => (
+        <label className="radio__container" key={value.name}>
+          {value.name}
+          <input
+            className="radio__input"
+            key={index}
+            type="radio"
+            name="color"
+            value={value.name}
+            // defaultChecked={index == 0 ? true : false}
+            onClick={() => setColor(value.name)}
+          />
+          {/* <span
+            className={`radio__style`}
+            style={{
+              background: `${color}`,
+              border: "2px solid white",
+              boxShadow: `${cor == color ? `0 0 0 2px ${color}` : "none"}`
+            }}
+          ></span> */}
+        </label>
+      ))}
+    </div>
+  );
+};
--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -6,6 +6,7 @@ import ProductDescription from "../components/Product/ProductDescription";
 import ProductPrice from "../components/Product/ProductPrice";
 import ProductTitle from "../components/Product/ProductTitle";
 import { SliderOther, SliderProduct } from "../components/Product/Slides";
+import { ProductColors } from "../components/ProductColors";
 import "./product.scss";
 
 interface ProductProps {}
@@ -19,6 +20,7 @@ export const Product: React.FC<ProductProps> = () => {
         <ProductTitle />
         <ProductDescription />
         <ProductPrice />
+        <ProductColors />
         <SliderProduct />
         <SliderOther />
         <PageTitle title={"Quem viu, viu tambem"} />
