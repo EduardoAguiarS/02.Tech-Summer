a2611104aea92120976e0bbd78ae024d8186626c
Eduardo Aguiar <edu.pessoal@outlook.com>
2022-05-18 13:11:55 -0300

    remove imports

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,14 +1,6 @@
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Hero";
-import PageTitle from "../components/PageTitle";
-import Breadcrumb from "../components/Product/Breadcrumb";
-import { ProductColors } from "../components/Product/ProductColors";
-import ProductDescription from "../components/Product/ProductDescription";
 import ProductDesk from "../components/Product/ProductDesk";
-import ProductPrice from "../components/Product/ProductPrice";
-import ProductSize from "../components/Product/ProductSize";
-import ProductTitle from "../components/Product/ProductTitle";
-import { SliderOther, SliderProduct } from "../components/Product/Slides";
 import "./product.scss";
 
 interface ProductProps {}
